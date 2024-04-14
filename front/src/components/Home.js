f2f44037253d1fdffdfe73ad7d56bae1f8f4adf3
Svetlana Djajic <svetlana.c.dj@gmail.com>
2024-04-14 20:33:45 +0200

    Create Home and NotFound pages

---
+++ front/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+    <div>
+    <h1>Welcome to Scooter Rental!</h1>
+    <img src="https://hips.hearstapps.com/hmg-prod/images/adult-scooter-are-scooters-good-exercise-1656691422.jpg?crop=0.902xw:0.676xh;0.0764xw,0.285xh&resize=1200:*"/>
+    </div>
+  )
+ 
+}
+
+export default Home;
